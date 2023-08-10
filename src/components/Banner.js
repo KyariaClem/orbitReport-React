1671c1f64cfad18b6bcb38bf8bb0cc55b691e55b
Kyaria Clemons <kyariaclemons@Kyarias-MBP.lan>
2023-08-09 23:03:29 -0500

    commiting and pushes changes for banner.js,buttons.js, satdta.js, table.js, testin.js

---
+++ src/components/Banner.js
@@ -0,0 +1,10 @@
+
+
+export default function Banner() {
+    return (
+        <div>
+            <h3>Orbit Report</h3>
+            <p>Click on the buttons to see the satellites in that orbit type</p>
+        </div>
+    )
+}
---
+++ src/components/testin.js
@@ -0,0 +1,9 @@
+
+export default function TestingFunction() {
+    return (
+        <div>
+            <h3>testing</h3>
+            <p>Click on the buttons to see the satellites in that orbit type</p>
+        </div>
+    )
+}
